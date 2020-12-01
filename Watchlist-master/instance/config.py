1d2f861c1b7badacc098c2373e677ea801823dfa
farzana583 <farzanaisack@gmail.com>
2020-12-01 11:58:08 +0300

    add

---
+++ Watchlist-master/instance/config.py
@@ -0,0 +1,2 @@
+MOVIE_API_KEY='349e511b84180acb3e41e139a07c85ed'
+SECRET_KEY='hakimziyech'
---
+++ Watchlist-master/start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='349e511b84180acb3e41e139a07c85ed'
+export SECRET_KEY='hakimziyech'
+
+python3.8 manage.py server
