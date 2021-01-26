90bbef66d991e28352484450b1ab8d6152ebfbcb
farzana583 <farzanaisack@gmail.com>
2021-01-26 21:49:44 +0300

    add ../Watchlist-master/app/auth/forms.py

--- Watchlist-master/app/auth/forms.py
+++ Watchlist-master/app/auth/forms.py
@@ -1,4 +1,4 @@
-rom flask_wtf import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
